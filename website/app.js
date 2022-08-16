9a1349c1ee09a27b7271c51c015016a471235095
zizo-bond <108358567+zizo-bond@users.noreply.github.com>
2022-08-16 22:42:36 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -2,32 +2,68 @@
 const baseURI = "https://api.openweathermap.org/data/2.5/weather?zip=";
 const requestForm = "https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}";
 
+//define personal API key
+const key = "&appid=036b6ffc6f93a9f7fd34f524747fd8f5&units=imperial";
 
-
-//for a better load performance
-setTimeout(() =>{
-    document.querySelector("body").style.opacity = '1';
-  },300);
-  
-
-
+//define variables
 const generate = document.querySelector("#generate");
-const country = document.getElementById('country');
 const zip = document.getElementById('zip');
 const feelings = document.getElementById('feelings');
-const coming = document.querySelector(".coming");
-const key = "&appid=036b6ffc6f93a9f7fd34f524747fd8f5&units=imperial";
 const temp = document.getElementById('temp');
 const content = document.getElementById('content');
-const city = document.getElementById('city');
-const weather = document.getElementById('weather');
 const date = document.getElementById('date');
-const errorMessage = document.getElementById('message');
-
+const city = document.getElementById('city');
+// Create a new date dynamically with JS
 let d = new Date();
 let newDate = d.toDateString();
 
 
+
+// event*
+//event listener on generate button
+generate.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const madeURL = `${baseURI}${zip.value},${country.value}${key}`;
+    //fetch the url and get the data the needs to be sliced
+    getData(madeURL).
+    then((data)=>{
+    //get the info we want from the coming data of the weather
+    projectData(data).
+    then((info)=>{
+    //post the data to a url : "/add" 
+    postData("/add", info).
+    then((data)=>{
+    //retrieve the data sent to the server from "/add" post 
+        retrieveData("/all").
+        then(data=>{
+    //  update the UI
+        updateUI(data);
+        });
+    });
+    });
+    
+    });
+});
+
+//postData method to send to the server
+const postData = async (url='', data={})=>{
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials:"same-origin",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(data)
+    });
+    try {
+        const result = await response.json();
+        return result;
+    }catch (err) {
+        console.error(error);
+    }
+};
+
+//api call method
 const getData = async (url) =>{
     try {   
            const response = await fetch(url);
@@ -41,7 +77,6 @@ const getData = async (url) =>{
         console.log(e.message);
     }
 };
-
 
 const projectData = async(data)=>{
     try{
@@ -64,25 +99,7 @@ const projectData = async(data)=>{
     }
 };
 
-
-const postData = async (url='', data={})=>{
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials:"same-origin",
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(data)
-    });
-    try {
-        const result = await response.json();
-        return result;
-    }catch (err) {
-        console.error(err);
-    }
-};
-
-
+  
 const retrieveData = async (url) => {
    const response = await fetch(url);
    try{
@@ -93,60 +110,27 @@ const retrieveData = async (url) => {
    }
 };
 
-const updateUI = async (info) => {
-    if(!info.message){
-        city.innerHTML = info.city + ", " + info.country;
-        weather.innerHTML = info.weather;
-        temp.innerHTML = `${info.temp}&#176`;
-        content.innerHTML = info.content? info.content  : "What do you feel &#128517;";
-        date.innerHTML = info.date;
-        message.innerHTML = "";
-        document.querySelector(".weather-info").style.opacity = "1";
-        setTimeout(async ()=> {
-        document.querySelector(".api-input").style.opacity = "1";
-            document.querySelector(".api-input").style.display="flex";
-            document.querySelector(".api-input").scrollIntoView();
-        }, 1000);
-        if(info.temp < 32){
-            document.querySelector("img").setAttribute("src", "./images/snow.png");
-        }else if(info.temp>80){
-            document.querySelector("img").setAttribute("src", "./images/hot.png");
-        }else{
-            document.querySelector("img").setAttribute("src", "https://freepngimg.com/thumb/weather/23698-6-weather-transparent-background.png");
-        }
-    }else{
-        document.querySelector(".weather-info").style.opacity = "1";
-        setTimeout(async ()=> {
-        document.querySelector(".api-input").style.opacity = "0";
-            document.querySelector(".api-input").style.display="none";
-            message.innerHTML = info.message;
-        },1000);
-   
+//updating the ui after receving response from the server
+const updateUI = async ()=> {
+  // console.log("updateUI"); //test
+    // wait response from the server get route
+    const req = await fetch('/all');
+    try{
+        const updatedData = await req.json();
+        console.log("data returned from the server"); //test
+        console.log(updatedData);
+        //update elements with the received data
+        document.getElementById('date').textContent = updatedData.date;
+        document.getElementById('temp').textContent = updatedData.temp.temp;
+        document.getElementById('city').textContent = updatedData.temp.name;
+        document.getElementById('content').textContent = updatedData.feeling;
     }
-};
-
-generate.addEventListener("click", (event)=>{
-    event.preventDefault();
-    const madeURL = `${baseURI}${zip.value},${country.value}${key}`;
-    //fetch the url and get the data the needs to be sliced
-    getData(madeURL).
-    then((data)=>{
-    //get the info we want from the coming data of the weather
-    projectData(data).
-    then((info)=>{
-    //post the data to a url called "/add" 
-    postData("/add", info).
-    then((data)=>{
-    //retrieve the data sent to the server from "/add" post 
-        retrieveData("/all").
-        then(data=>{
-    // now am able to update the ui with the data i got
-        updateUI(data);
-        });
-    });
-    });
-    
-    });
-});
-
- 
+    catch(error){
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+ //loading the page smoothly
+setTimeout(() =>{
+    document.querySelector("body").style.opacity = '1';
+  },200);
